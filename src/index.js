7385cece3b6395041368426fa0f3bfe4e4a00d9c
Ashwini Surve <ashwinisurve@Ashwinis-MacBook-Air.local>
2021-06-14 06:53:49 +0530

    tic tac toe commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,112 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import ReactDOM from "react-dom";
+import { useState } from "react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Board(props) {
+  const renderSquare = (i) => {
+    return (
+      <Square
+        value={props.squares[i]}
+        onClick={() => {
+          props.handleClick(i);
+        }}
+      />
+    );
+  };
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+function Game(props) {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [current, setCurrent] = useState(history[history.length - 1]);
+  const winner = calculateWinner(current.squares);
+  const status = winner
+    ? `winner ${winner}`
+    : `Next player: ${xIsNext ? "X" : "O"}`;
+
+  const handleClick = (i) => {
+    let updatedSquares = [...current.squares];
+    if (calculateWinner(current.squares) || current.squares[i]) {
+      return;
+    }
+    updatedSquares[i] = xIsNext ? "X" : "O";
+    setXIsNext(!xIsNext);
+    setCurrent({ squares: updatedSquares });
+    setHistory([...history, { squares: updatedSquares }]);
+  };
+
+  const jumpTo = (move) => {
+    setCurrent(history[move]);
+  };
+
+  const moves = history.map((step, move) => {
+    const description = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} handleClick={(i) => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
